841fe94d87dfc8d5415da5a87700841ee4e2767f
Toni Robres <toni.robres@scrm.lidl>
2025-01-24 11:10:59 +0100

    fix

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ app.include_router(address.router)
 app.include_router(users.router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
